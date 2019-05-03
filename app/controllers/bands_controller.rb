09665a01af734b33dd4bf3357d707632024ed6c4
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-05-02 20:29:12 -0400

    modify bands_controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -17,6 +17,17 @@ class BandsController < ApplicationController
         render :new
     end
 
+    def edit
+    end
+    
+    def update
+    end
+
+    def destroy
+        b = Band.find_by(id: params[:id])
+        b.delete
+    end
+
     private
     def band_params
         params.require(:band).permit(:name)
